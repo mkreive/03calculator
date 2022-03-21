56026e245b8a89ac19511d5528ddaf70a4f5d6a7
mkreive <mkreive@gmail.com>
2022-03-21 15:23:52 +0200

    selectors redone

--- script.js
+++ script.js
@@ -4,26 +4,14 @@
 const actionsElement = document.getElementById("actions");
 const answerElement = document.getElementById("answer");
 
-const one = document.getElementById("1");
-const two = document.getElementById("2");
-const three = document.getElementById("3");
-const four = document.getElementById("4");
-const five = document.getElementById("5");
-const six = document.getElementById("6");
-const seven = document.getElementById("7");
-const eight = document.getElementById("8");
-const nine = document.getElementById("9");
-const zero = document.getElementById("0");
-const doubleZero = document.getElementById("00");
+const numberButtons = document.querySelectorAll(".number");
 
-const clearButton = document.getElementById("clear");
-const percentagesButton = document.getElementById("percentages");
-const backButton = document.getElementById("back");
-const equalButton = document.getElementById("equal");
-const dotButton = document.getElementById("dot");
-const multiplicationButton = document.getElementById("multiplication");
-const minusButton = document.getElementById("minus");
-const plusButton = document.getElementById("plus");
-const divide = document.getElementById("divide");
+const actionButtons = document.querySelectorAll(".actions");
 
 // Code
+
+for (let button of numberButtons) {
+    button.addEventListener("click", function () {
+        console.log(button.innerText);
+    });
+}
