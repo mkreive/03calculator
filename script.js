bf12715081b96c584caafe820441d51b47e62881
mkreive <mkreive@gmail.com>
2022-03-22 16:25:01 +0200

    something

--- script.js
+++ script.js
@@ -10,38 +10,26 @@ const buttons = document.querySelectorAll("button");
 for (let button of buttons) {
     button.addEventListener("click", function () {
         let clickedButton = button.innerText;
+        prevButton = button;
         let inputValue = inputElement.innerHTML;
         inputElement.innerText += clickedButton;
+        console.log(clickedButton);
+
+        // if (button.classList.contains("actions") &&)
 
         if (clickedButton === "C") {
             inputElement.innerText = "";
+            answerElement.innerText = "";
         } else if (clickedButton === "=") {
             inputElement.innerText = inputValue.split(0, -1);
-            const equation = inputValue;
-            console.log(equation);
-            // const lastChar = equation[equation.length - 1];
+            let equation = inputValue;
+
             equation = equation
                 .replace(/x/g, "*")
                 .replace(/÷/g, "/")
                 .replace(/\^/g, "**");
+            console.log(equation);
+            answerElement.innerText = eval(equation);
         }
-
-        // if (button.classList.contains("actions")) {
-        //     actionsArray.push(inputElement.innerText.slice(0, -1));
-
-        //     console.log(actionsArray);
-        // }
-        // if (button.innerText === "=") {
-        //     actionsString = inputElement.innerText.slice(0, -1).split("");
-        //     inputElement.innerText = "";
-        //     console.log(actionsString);
-
-        //     for (let i = 0; i < actionsString.length; i++) {
-        //         if (actionsString[i] === "/") {
-        //             const answer = actionsString[i - 1] / actionsString[i + 1];
-        //             console.log(answer);
-        //         }
-        //     }
-        // }
     });
 }
